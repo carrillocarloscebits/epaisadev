83722e7d4f7acf7fa01ca5039750841ef1dd9634
f <f10h09z91p@gmail.com>
2018-12-26 10:15:30 -0500

    independent cash-register

--- android/app/src/main/java/com/project_1/MainApplication.java
+++ android/app/src/main/java/com/project_1/MainApplication.java
@@ -3,6 +3,11 @@ package com.project_1;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.horcrux.svg.SvgPackage;
+import org.wonday.orientation.OrientationPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -25,9 +30,12 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
-            new LinearGradientPackage()
+            new SvgPackage(),
+            new OrientationPackage(),
+            new LinearGradientPackage(),
+            new RNDeviceInfo(),
+            new RNGestureHandlerPackage()
       );
     }
 

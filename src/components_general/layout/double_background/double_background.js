a14673184911975400f63052f3a1b4f98ad49457
f <f10h09z91p@gmail.com>
2019-01-03 10:26:03 -0500

    new background image

--- src/components_general/layout/double_background/double_background.js
+++ src/components_general/layout/double_background/double_background.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Image, Dimensions } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import backgroung_portrait from './assets/backgroung_portrait.png';
-import background_image_login_landscape from './assets/background_image_login_landscape_medium.jpg';
+import background_image_login_landscape from './assets/background_image_login_landscape_medium2.jpg';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -31,7 +31,7 @@ class DoubleBackground extends Component {
           <Image
             source={background_image_login_landscape}
             style={{ height: hp('115%'), width: '100%' }}
-            resizeMode={'stretch'}
+            resizeMode={'cover'}
           />
         )}
 
